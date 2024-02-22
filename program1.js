b29407afe650f1d877b2fa0a1e3828ad835e4b4b
roysudeep191 <154358330+roysudeep191@users.noreply.github.com>
2024-02-22 13:22:50 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,34 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isBalanced(str) {
+    const stack = [];
+    const openingBrackets = ['(', '{', '['];
+    const closingBrackets = [')', '}', ']'];
+    const bracketPairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
 
-module.exports = { isValid };
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        if (openingBrackets.includes(char)) {
+            stack.push(char);
+        } else if (closingBrackets.includes(char)) {
+            const lastOpeningBracket = stack.pop();
+            if (lastOpeningBracket !== bracketPairs[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+
+const expression = "{[()]}";
+console.log(isBalanced(expression)); 
+
+const expression2 = "{[(])}";
+console.log(isBalanced(expression2)); 
+
 
 
