62942b16595821812a1e820146d1987eb2bbebbd
roysudeep191 <154358330+roysudeep191@users.noreply.github.com>
2024-02-22 13:24:35 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,27 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanNumerals = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentNumeral = romanNumerals[s[i]];
+        const nextNumeral = romanNumerals[s[i + 1]];
 
-module.exports={romanToInt}
+        if (nextNumeral && currentNumeral < nextNumeral) {
+            result += nextNumeral - currentNumeral;
+            i++;
+        } else {
+            result += currentNumeral;
+        }
+    }
+
+    return result;
+}
+
